d1168d919288c75b9aa1d721ab78279584e44f3a
Harshitha-20-11-2005 <kandepuharshitha688@gmail.com>
2025-02-15 14:19:29 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,4 @@
+print("Welcome to the brand Name generator")
+var1=input("what's the name name of the city you grew up in?")
+var2=input("what's your pet name?")
+print("Your band name could be "+var1+"  "+var2)
